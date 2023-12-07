ee3901c4065f7afcf6d700455b5a6095f0adfbad
omarelwaliely <77898546+omarelwaliely@users.noreply.github.com>
2023-12-07 18:09:10 +0200

    fixed peak bugs

--- spikeSort.py
+++ spikeSort.py
@@ -15,13 +15,20 @@ def extractFeatures(df,windowSize):
    i = 0
    while i < len(arr):
       if arr[i] > threshold:
-         start = max(0, i - windowSize // 2)
-         end = min(len(arr), i + windowSize // 2)
+         j = i
+         maximum = arr[i]
+         maxInd = i
+         while(arr[j] > threshold):
+            if arr[j] > maximum:
+               maximum = arr[j]
+               maxInd = j
+            j+=1
+         start = max(0, maxInd - windowSize // 2)
+         end = min(len(arr), maxInd + windowSize // 2)
          window = arr[start:end]
-
          std = np.std(window)
          diff = np.max(np.abs(np.diff(window)))
-         featureList.append({'std': std, 'diff': diff,'peakIndex' : i })
+         featureList.append({'std': std, 'diff': diff,'peakIndex' : maxInd })
          i = end
       else:
          i += 1
@@ -31,23 +38,22 @@ def plotFeatures(features):
    X = features[['diff', 'std']]
    kmeans = KMeans(n_clusters=2)
    features['cluster'] = kmeans.fit_predict(X)
-   plt.scatter(features['std'], features['diff'], c=features['cluster'], cmap='viridis')
+   plt.scatter(features['std'], features['diff'], c=features['cluster'], cmap='plasma')
    plt.xlabel('Standard Deviation')
-   plt.ylabel('Maximum Difference between Two Succesive Samples')
+   plt.ylabel('Maximum Difference Between Two Succesive Samples')
    plt.title('K-Means Visualization')
    plt.show()
 
 def main():
    windowSize = 0.002 * 24414
    windowSize = int(windowSize)
-   print(windowSize)
    first = pd.DataFrame()
    second = pd.DataFrame()
    first, second = splitColumns("Data.txt")
    features1 = extractFeatures(first,windowSize)
-   # features2 = extractFeatures(second,windowSize)
-   # features2.to_csv("features2.csv", index = False)  
+   features2 = extractFeatures(second,windowSize)
    plotFeatures(features1)
+   plotFeatures(features2)
    features1.to_csv("features1.csv",index = False)
-   # plotFeatures(features2)
+   features2.to_csv("features2.csv", index = False)  
 main()
