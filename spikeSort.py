1368f1eaf88bf5e05cf64f4c2f6cf503454226d2
Seifeldin Elshabshiri <seifelshabshiri@aucegypt.edu>
2023-12-08 19:23:43 +0200

    plotSignal by Freddy, plotAverageNeurons by Seif

--- spikeSort.py
+++ spikeSort.py
@@ -44,6 +44,74 @@ def plotFeatures(features):
    plt.title('K-Means Visualization')
    plt.show()
 
+
+def plotSignal(Osignal,fileName,windowSize):#Plotting the signal mapping the paeak with a red colored point
+     
+   # Read the DataFrame from the CSV file
+   df = pd.read_csv(fileName)
+
+   # Extract peaks and clusters from the dataframe
+   peaks = df['peakIndex'].values
+   clusters = df['cluster'].values
+   unique_clusters = np.unique(clusters)
+
+   # Plot the signal
+   signal = Osignal.to_numpy()  # Replace 'YourSignalColumn' with the actual column name
+   plt.plot(signal, label='Signal')
+   cluster_colors = ['red', 'green', 'yellow']  # Add more colors as needed
+
+   # Plot peaks with different colors for each cluster
+   for i, cluster in enumerate(unique_clusters):
+      cluster_peaks = peaks[clusters == cluster]
+      plt.scatter(cluster_peaks, signal[cluster_peaks], label=f'Cluster {cluster}', color=cluster_colors[i], marker='o')
+
+   # Customize the plot
+   plt.title('Signal with Clustered Peaks')
+   plt.xlabel('Peak')
+   plt.ylabel('Amplitude')
+   plt.legend()
+
+   # Show the plot
+   plt.show()
+
+def plotFeatures(features):
+   plt.scatter(features['std'], features['diff'], c=features['cluster'], cmap='plasma')
+   plt.xlabel('Standard Deviation')
+   plt.ylabel('Maximum Difference Between Two Succesive Samples')
+   plt.title('K-Means Visualization')
+   plt.show()
+   return features
+
+def plotAverageNeurons(feauture, data):
+   allSpikes1 = []
+   for ind in feauture[feauture["cluster"] == 0]["peakIndex"]:
+    start = ind - 20
+    end = ind + 20
+    allSpikes1.append([data[start:end]])
+   allSpikes2 = []
+   for ind in feauture[feauture["cluster"] == 1]["peakIndex"]:
+    start = ind - 20
+    end = ind + 20
+    allSpikes2.append([data[start:end]])
+   avg_spikes = []
+   avg_spikes.append(np.average(allSpikes1, axis = 0)[0])
+   avg_spikes.append(np.average(allSpikes2, axis = 0)[0])
+   indices = np.arange(len(avg_spikes[0]))
+   plt.plot(indices, avg_spikes[0], color='blue', label="neuron one")
+   plt.plot(indices, avg_spikes[1], color='red', label="neuron two")
+   plt.xlabel('Time')
+   plt.ylabel('Amplitude')
+   plt.title('Single Spike Waveform')
+   plt.legend()
+   plt.grid(True)
+   plt.show()
+
+def addClusters(features):
+   X = features[['diff', 'std']]
+   kmeans = KMeans(n_clusters=2)
+   features['cluster'] = kmeans.fit_predict(X)
+   return features
+
 def main():
    windowSize = 0.002 * 24414
    windowSize = int(windowSize)
@@ -51,9 +119,16 @@ def main():
    second = pd.DataFrame()
    first, second = splitColumns("Data.txt")
    features1 = extractFeatures(first,windowSize)
-   features2 = extractFeatures(second,windowSize)
+   features1 = addClusters(features1)
    plotFeatures(features1)
+   plotSignal(first,"features1.csv",windowSize)
+   plotAverageNeurons(features1, first)
+
+   features2 = extractFeatures(second,windowSize)
+   features2 = addClusters(features2)
    plotFeatures(features2)
-   features1.to_csv("features1.csv",index = False)
-   features2.to_csv("features2.csv", index = False)  
+   plotAverageNeurons(features2, second)
+
+   #features1.to_csv("features1.csv",index = False)
+   #features2.to_csv("features2.csv", index = False)  
 main()
